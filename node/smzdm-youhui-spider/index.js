65efb7a3840d0f9bcfea98e24d64ffcd0af7bf7b
wzhkl <1667206227@qq.com>
2019-05-21 19:16:16 +0800

    spider smzdm-youhui

---
+++ node/smzdm-youhui-spider/index.js
@@ -0,0 +1,22 @@
+const request = require('request');
+const cheerio = require('cheerio')
+
+request('https://www.smzdm.com/youhui/', (err, res) => {
+    if(!err) {
+        // console.log(res.body);
+        // 要不要解析 html entity
+        const $ = cheerio.load(res.body, {
+            decodeEntities: false
+        })
+        $('.list.list_preferential').each(function() {
+            let title = $('.itemName a',this).html();
+            title = title.replace(/<.*>.*<\/.*>/g, '');
+            const price = $('.listTitle .red',this).text();
+            const img = $('.picLeft img',this).attr('src');
+            const desc = $('.lrInfo p',this).text().trim();
+            console.log({
+                title,price,img,desc
+            });
+        })
+    }
+})
