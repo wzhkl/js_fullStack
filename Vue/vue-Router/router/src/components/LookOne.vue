8ed24fc2b3281072e3d5f4692ef1ed1be295a6e7
wzhkl <1667206227@qq.com>
2019-06-04 19:45:46 +0800

    路由传参

---
+++ Vue/vue-Router/router/src/components/LookOne.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="look">{{message}}
+    {{$route.params.name}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Look',
+  data () {
+    return {
+      message: '别看了'
+    }
+  }
+}
+</script>
---
+++ Vue/vue-Router/router/src/components/LookTwo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="look">{{message}}
+    <p>接受的参数: {{ $route.params.userId }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Look',
+  data () {
+    return {
+      message: '就要看'
+    }
+  }
+}
+</script>
