581f90046d5d29c697e126883f55b54d9a9f579c
wzhkl <1667206227@qq.com>
2019-06-12 15:08:22 +0800

    删除了误上传的node_modules文件

---
+++ webpack/babel/index.js
@@ -0,0 +1,10 @@
+const a = 1, 
+    b = 2, 
+    c = a + b;
+console.log(c);
+
+[1,2,3].map(item => `${item}item`);
+
+// es6的大部分功能，es5也能实现，
+let xkz = {name: '陈祖康', hometown: '高安'}
+xkz = {...xkz, "company": '晖哥有限公司'}
