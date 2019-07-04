ac7e97006781bda3eba6f261f516e256501cc3af
wzhkl <1667206227@qq.com>
2019-07-04 16:36:15 +0800

    组件传值进阶

---
+++ Vue/vue-components/src/components/com/menu.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="menu">
+    {{message}}
+    {{name.name}}
+    {{app.user}}
+  </div>
+</template>
+
+<script>
+import bus from '@/bus/bus'
+export default {
+  inject: ['name', 'app'],
+  data () {
+    return {
+      message: '我是menu'
+    }
+  },
+  created() {
+    let self = this
+    bus.$on('todo', (val) => {
+      console.log(val)
+    })
+  }
+}
+</script>
+
+<style>
+
+</style>
