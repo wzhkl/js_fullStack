b6034464cbef94454327eee41749d7524bc0222a
wzhkl <1667206227@qq.com>
2019-07-05 20:58:34 +0800

    vue-music

---
+++ Vue/vue-music/src/common/dom.js
@@ -0,0 +1,28 @@
+let elementStyle = document.createElement('div').style
+let vendor = (() => {
+  let transfromNames = {
+    webkit: 'webkitTransform',
+    Moz: 'MozTransform',
+    O: 'OTransform',
+    ms: 'msTransform',
+    standard: 'transform'
+  }
+
+  for (let key in transfromNames) {
+    if (elementStyle[transfromNames[key]] !== undefined) {
+      return key
+    }
+  }
+  return false
+})()
+
+export function prefixStyle(style) {
+  if (vendor === false) {
+    return false
+  }
+  if (vendor === 'standard') {
+    return style
+  }
+
+  return vendor + style.charAt(0).toUpperCase() + style.substr(1)
+}
---
+++ Vue/vue-music/src/vuex/types.js
@@ -0,0 +1,8 @@
+export const COM_SHOW_SIDE_BAR = 'COM_SHOW_SIDE_BAR' //边侧栏
+export const SET_FULL_SCREEN = 'SET_FULL_SCREEN' // 设置播放器是否全屏展示
+export const COM_SAVE_SEARCH_HISTORY = 'COM_SAVE_SEARCH_HISTORY' // 保存搜索历史
+export const SET_PLAYLIST = 'SET_PLAYLIST' // 播放列表
+export const SET_CURRENT_INDEX = 'SET_CURRENT_INDEX' // 设置播放音乐的索引
+export const SET_PLAYING = 'SET_PLAYING' // 控制歌曲播放状态
+export const SAVE_PALY_HISTORY = 'SAVE_PALY_HISTORY' // 保存播放过的歌曲
+export const SAVE_FAVORITE_LIST = 'SAVE_FAVORITE_LIST' // 保存喜欢歌曲列表
