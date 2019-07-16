912884d4fcf3de397136d360d4764d2a1d212442
wzhkl <1667206227@qq.com>
2019-07-16 19:16:11 +0800

    react与redux结合，使用react-redux中Provide和connect

--- React/redux-counter/src/index.js
+++ React/redux-counter/src/index.js
@@ -1,74 +1,17 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux'
-
-function reducer(state, action) {
-  if (action.type === 'INCREMENT') {
-    return state + 1;
-  } else if (action.type === 'DECREMENT') {
-    return state - 1;
-  } else {
-    return 0;
-  }
-}
-
-function filmReducer(state = [], action) {
-  switch (action.type) {
-    case 'ADD_FILM':
-      return [...state, action.film];
-    default :
-      return state;
-  }
-}
-const index = combineReducers({
-  count: reducer,
-  films: filmReducer
-})
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import index from './reducer/index.js';
+import Count from './App.js'
 
 const store = createStore(index);
-class Count extends Component {
-  state = {  }
-  handleIn = () => {
-    store.dispatch({
-      type: 'INCREMENT'
-    })
-  }
-  handleDe = () => {
-    store.dispatch({
-      type: 'DECREMENT'
-    })
-  }
-  handleAddFilm = () => {
-    store.dispatch({
-      type: 'ADD_FILM',
-      film: { name: '霸王别姬', id: 0 }
-    })
-  }
-  render() {
-    return (
-      <div>
-        count: {store.getState().count};<br/>
-        <button onClick={this.handleIn}>+</button>
-        <button onClick={this.handleDe}>-</button><br/>
-        <button onClick={this.handleAddFilm}>添加电影</button>
-        {
-          store.getState().films.map((film, i) => {
-            return (
-              <li key={i}>{ film.name }</li>
-            )
-          })
-        }
-      </div>
-    );
-  }
-}
- 
-export default Count;
-ReactDOM.render(<Count />, document.getElementById('root'));
-store.subscribe(() => {
-  ReactDOM.render(<Count />, document.getElementById('root'));
-})
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(
+<Provider store={store}>
+  <Count />
+</Provider>, document.getElementById('root'));
+// store.subscribe(() => {
+//   ReactDOM.render(<Count />, document.getElementById('root'));
+// })
+
---
+++ React/redux-counter/src/reducer/index.js
@@ -0,0 +1,27 @@
+import { combineReducers } from 'redux'
+
+function reducer(state, action) {
+  if (action.type === 'INCREMENT') {
+    return state + 1;
+  } else if (action.type === 'DECREMENT') {
+    return state - 1;
+  } else {
+    return 0;
+  }
+}
+
+function filmReducer(state = [], action) {
+  switch (action.type) {
+    case 'ADD_FILM':
+      return [...state, action.film];
+    default :
+      return state;
+  }
+}
+
+const index = combineReducers({
+  count: reducer,
+  films: filmReducer
+})
+
+export default index
