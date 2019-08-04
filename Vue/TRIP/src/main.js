54de51136b9a52e102dcad11592fe063e0ab33bb
wzhkl <1667206227@qq.com>
2019-08-04 16:10:35 +0800

    TRIP and TRIP-server

---
+++ Vue/TRIP/src/main.js
@@ -0,0 +1,31 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import mandMobile from 'mand-mobile'
+import 'mand-mobile/lib/mand-mobile.css'
+import store from './store/index'
+import AMap from 'vue-amap'
+
+Vue.use(AMap)
+AMap.initAMapApiLoader({
+  // 高德的key
+  key: '',
+  plugin: ['AMap.Autocomplete', 'AMap.PlaceSearch', 'AMap.Scale', 'AMap.OverView', 'AMap.ToolBar', 'AMap.MapType', 'AMap.PolyEditor', 'AMap.CircleEditor']
+})
+Vue.use(mandMobile)
+// 请求拦截器
+import requestPlugin from './request/http.js'
+Vue.use(requestPlugin)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App/>'
+})
